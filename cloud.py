3faa45756a177a59ad1185b2ee57af06c5285aab
Gerardo González Becerril <a01411981@itesm.mx>
2020-12-14 22:04:33 -0600

    improved UI with bootstrap

--- cloud.py
+++ cloud.py
@@ -8,7 +8,7 @@ def create_wordcloud(text, name, tempdir):
 	currdir = path.dirname(__file__)
 	mask = np.array(Image.open(path.join(currdir, "static/cloud.png")))
 	stopwords = set(STOPWORDS)
-	wc = WordCloud(background_color="white",
+	wc = WordCloud(background_color="#F9F9F9",
 					max_words=200, 
 					mask=mask,
 	               	stopwords=stopwords)
